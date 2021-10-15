24709b091611f43f3ebdcf41e3371599da5b9c5d
Stephen Astels <sastels@gmail.com>
2021-10-15 11:12:14 -0400

    refactor

--- Labs/6.13: Using EC2 Roles and Instance Profiles in AWS/iam.tf
+++ Labs/6.13: Using EC2 Roles and Instance Profiles in AWS/iam.tf
@@ -1,25 +1,19 @@
 resource "aws_iam_role" "web_role" {
   name = "web_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-
-  tags = {
-    tag-key = "tag-value"
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
 }
 
 resource "aws_iam_instance_profile" "web_profile" {
@@ -31,18 +25,16 @@ resource "aws_iam_role_policy" "web_policy" {
   name = "web_policy"
   role = aws_iam_role.web_role.id
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "s3:*"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
 }
