03011143827982700dc052456f75349e619ce3f4
Stephen Astels <sastels@gmail.com>
2021-10-15 11:24:29 -0400

    readme

---
+++ Labs/6.13: Using EC2 Roles and Instance Profiles in AWS/README.md
@@ -0,0 +1,6 @@
+
+### Usefule links
+
+https://devopslearning.medium.com/aws-iam-ec2-instance-role-using-terraform-fa2b21488536
+
+
--- Labs/6.13: Using EC2 Roles and Instance Profiles in AWS/provider.tf
+++ Labs/6.13: Using EC2 Roles and Instance Profiles in AWS/provider.tf
@@ -1,5 +1,3 @@
-# https://devopslearning.medium.com/aws-iam-ec2-instance-role-using-terraform-fa2b21488536
-
 terraform {
   required_providers {
     aws = {
