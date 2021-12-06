d8a38d5b300e33e48c22ce740988cc1278c3ee03
Stephen Astels <sastels@gmail.com>
2021-12-06 09:29:27 -0500

    add dedicated k8s route

--- Misc/DNS canary testing/variables.tf
+++ Misc/DNS canary testing/variables.tf
@@ -1,16 +1,14 @@
-
-variable "name" {
-  type = string
-}
-
 variable "zone_id" {
-  type = string
+  type    = string
+  default = "Z03787061QOM2973GWMV4"
 }
 
 variable "notification_alb" {
-  type = string
+  type    = string
+  default = "notification-staging-alb-1878361959.ca-central-1.elb.amazonaws.com"
 }
 
 variable "api_lambda_app_gateway" {
-  type = string
+  type    = string
+  default = "74i43aysii.execute-api.ca-central-1.amazonaws.com"
 }
