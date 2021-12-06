4dfcaa38af3bd5d0a32590227f738bb179a201dc
Stephen Astels <sastels@gmail.com>
2021-12-06 14:50:45 -0500

    create weighted records

--- Misc/DNS canary testing/route53.tf
+++ Misc/DNS canary testing/route53.tf
@@ -1,11 +1,15 @@
-resource "aws_route53_record" "api" {
-  zone_id = var.zone_id
-  name    = "api.staging.notification.cdssandbox.xyz"
-  type    = "CNAME"
-  records = [var.notification_alb]
-  ttl     = "60"
-}
+# original record
 
+# resource "aws_route53_record" "api" {
+#   zone_id = var.zone_id
+#   name    = "api.staging.notification.cdssandbox.xyz"
+#   type    = "CNAME"
+#   records = [var.notification_alb]
+#   ttl     = "60"
+# }
+
+
+# direct k8s / lambda records
 resource "aws_route53_record" "api-k8s" {
   zone_id = var.zone_id
   name    = "api-k8s.staging.notification.cdssandbox.xyz"
@@ -25,26 +29,28 @@ resource "aws_route53_record" "api-k8s" {
 # }
 
 
-# Do next!
-#
-# resource "aws_route53_record" "api-k8s-weighted" {
-#   zone_id = var.zone_id
-#   name    = "api.staging.notification.cdssandbox.xyz"
-#   type    = "CNAME"
-#   records = [var.notification_alb]
-#   ttl     = "60"
-#   weighted_routing_policy {
-#     weight = 255
-#   }
-# }
-#
-# resource "aws_route53_record" "api-lambda-weighted" {
-#   zone_id = var.zone_id
-#   name    = "api.staging.notification.cdssandbox.xyz"
-#   type    = "CNAME"
-#   records = [var.api_lambda_app_gateway]
-#   ttl     = "60"
-#   weighted_routing_policy {
-#     weight = 0
-#   }
-# }
+# Weighted records
+
+resource "aws_route53_record" "api-k8s-weighted" {
+  zone_id        = var.zone_id
+  name           = "api.staging.notification.cdssandbox.xyz"
+  type           = "CNAME"
+  records        = [var.notification_alb]
+  ttl            = "60"
+  set_identifier = "loadbalancer"
+  weighted_routing_policy {
+    weight = 255
+  }
+}
+
+resource "aws_route53_record" "api-lambda-weighted" {
+  zone_id        = var.zone_id
+  name           = "api.staging.notification.cdssandbox.xyz"
+  type           = "CNAME"
+  records        = ["api-lambda.staging.notification.cdssandbox.xyz"]
+  ttl            = "60"
+  set_identifier = "lambda"
+  weighted_routing_policy {
+    weight = 0
+  }
+}
