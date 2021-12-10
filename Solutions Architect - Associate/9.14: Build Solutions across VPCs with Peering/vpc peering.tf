301f5eaaf0272a037744e0f630519121d78f1edb
Stephen Astels <sastels@gmail.com>
2021-12-10 16:49:05 -0500

    cleanup

--- Solutions Architect - Associate/9.14: Build Solutions across VPCs with Peering/vpc peering.tf
+++ Solutions Architect - Associate/9.14: Build Solutions across VPCs with Peering/vpc peering.tf
@@ -18,27 +18,3 @@ resource "aws_route" "db" {
   destination_cidr_block    = aws_vpc.Web_VPC.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.DBtoWeb.id
 }
-
-
-
-# resource "aws_route" "primary2secondary" {
-#   # ID of VPC 1 main route table.
-#   route_table_id = aws_vpc.Web_VPC.main_route_table_id
-
-#   # CIDR block / IP range for VPC 2.
-#   destination_cidr_block = aws_vpc.DB_VPC.cidr_block
-
-#   # ID of VPC peering connection.
-#   vpc_peering_connection_id = aws_vpc_peering_connection.primary2secondary.id
-# }
-
-# resource "aws_route" "secondary2primary" {
-#   # ID of VPC 2 main route table.
-#   route_table_id = aws_vpc.DB_VPC.main_route_table_id
-
-#   # CIDR block / IP range for VPC 1.
-#   destination_cidr_block = aws_vpc.Web_VPC.cidr_block
-
-#   # ID of VPC peering connection.
-#   vpc_peering_connection_id = aws_vpc_peering_connection.primary2secondary.id
-# }
