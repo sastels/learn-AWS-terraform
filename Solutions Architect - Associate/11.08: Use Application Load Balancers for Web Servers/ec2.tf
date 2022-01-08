fa153d0186cc8062eed90e02f65820008db58506
Stephen Astels <sastels@gmail.com>
2022-01-07 21:28:50 -0500

    3 web servers

--- Solutions Architect - Associate/11.08: Use Application Load Balancers for Web Servers/ec2.tf
+++ Solutions Architect - Associate/11.08: Use Application Load Balancers for Web Servers/ec2.tf
@@ -1,32 +1,37 @@
-resource "aws_instance" "web_server_1" {
+variable "num_servers" {
+  default = 3
+}
+
+resource "aws_instance" "web_server" {
+  count                       = var.num_servers
   ami                         = "ami-0d8ad3ab25e7abc51" # Amazon Linux 2
-  # ami                         = "ami-0bb84e7329f4fa1f7" # Ubuntu Server 20.04 LTS 
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.ec2.id]
   associate_public_ip_address = true
   key_name                    = "ssh-key"
-  user_data                   = data.template_file.bootstrap.rendered
+  user_data                   = element(template_file.bootstrap.*.rendered, count.index)
   subnet_id                   = aws_subnet.WebPublic.id
 
   root_block_device {
     encrypted = true
   }
- tags = {
-    Name = "Web server 1"
+  tags = {
+    Name = "Web server ${count.index}"
   }
   depends_on = [aws_internet_gateway.WebIG]
 }
 
-data "template_file" "bootstrap" {
+resource "template_file" "bootstrap" {
+  count    = var.num_servers
   template = file("./bootstrap.sh")
   vars = {
-    server_id     = "1"
+    server_id = "${count.index}"
   }
 }
 
 output "curl" {
-  value = "curl ${aws_instance.web_server_1.public_dns}\n"
+  value = "curl ${aws_instance.web_server[0].public_dns}\n"
 }
 output "ssh" {
-  value = "ssh -i ~/.ssh/id_rsa ec2-user@${aws_instance.web_server_1.public_dns}\n"
+  value = "ssh -i ~/.ssh/id_rsa ec2-user@${aws_instance.web_server[0].public_dns}\n"
 }
