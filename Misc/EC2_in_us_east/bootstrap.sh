3fa02d20734e3ec53e8b9668dac08d3ddc718784
Stephen Astels <sastels@gmail.com>
2022-08-04 14:28:51 -0400

    run a server in us-east-1

---
+++ Misc/EC2_in_us_east/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo su - root
+
+# update
+
+yum update -y
+
+# install other packages
+
---
+++ Misc/EC2_in_us_east/security_group.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "ec2" {
+  name        = "ec2_security_group"
+  description = "EC2 security group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
