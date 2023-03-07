b2dae2fc9d574781130fd4d82cb3ff73e56bc99a
Stephen Astels <sastels@gmail.com>
2023-03-07 12:00:55 -0500

    apache server working

--- Misc/Notify callback/bootstrap.sh
+++ Misc/Notify callback/bootstrap.sh
@@ -5,6 +5,12 @@ sudo su - root
 
 yum update -y
 
+# Python
+
+pip3 install flask
+
+# app
+
 cat > ./application.py <<EOF
 from flask import Flask
 app = Flask(__name__)
@@ -14,33 +20,36 @@ def hello_world():
     return 'Hello, World!'
 EOF
 
-pip3 install flask
+# run
 
-# flask --app /application.py run
+# flask --app /application.py run --port=80
+
+
+# Apache!!
 
 # install other packages
 
-# amazon-linux-extras install -y php7.2
-# yum install -y httpd mysql
+amazon-linux-extras install -y php7.2
+yum install -y httpd mysql
 
-# # tweak groups and file ownership
+# tweak groups and file ownership
 
-# usermod -a -G apache ec2-user
-# chown -R ec2-user:apache /var/www
-# chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
-# find /var/www -type f -exec sudo chmod 0664 {} \;
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
 
-# # start apache
+# start apache
 
-# systemctl start httpd
-# systemctl enable httpd
+systemctl start httpd
+systemctl enable httpd
 
-# # simple home page
+# simple home page
 
-# cat > /var/www/html/index.html <<EOF
-# <h1> My web server!! </h1>
-# Go to <a href="/wordpress">Wordpress</a>
-# EOF
+cat > /var/www/html/index.html <<EOF
+<h1> My web server!! </h1>
+Go to <a href="/wordpress">Wordpress</a>
+EOF
 
 # # Wordpress
 
