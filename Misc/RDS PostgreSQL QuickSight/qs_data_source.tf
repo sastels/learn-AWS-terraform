fa9edfa70488d8a73940234e59f92bdf745fcf66
Stephen Astels <sastels@gmail.com>
2023-09-29 15:26:08 -0400

    try data source

--- Misc/RDS PostgreSQL QuickSight/qs_data_source.tf
+++ Misc/RDS PostgreSQL QuickSight/qs_data_source.tf
@@ -1,22 +1,22 @@
-# resource "aws_quicksight_data_source" "default" {
-#   depends_on     = [aws_iam_role_policy_attachment.rds-qs-attach]
-#   data_source_id = "dvdrental"
-#   name           = "DVD rentals demo dataset"
+resource "aws_quicksight_data_source" "default" {
+  depends_on     = [aws_iam_role_policy_attachment.rds-qs-attach]
+  data_source_id = "dvdrental"
+  name           = "DVD rentals demo dataset"
 
-#   credentials {
-#     credential_pair {
-#       username = aws_db_instance.test.username
-#       password = aws_db_instance.test.password
-#     }
-#   }
-#   vpc_connection_properties {
-#     vpc_connection_arn = aws_quicksight_vpc_connection.example.arn
-#   }
-#   parameters {
-#     rds {
-#       database    = "dvdrental"
-#       instance_id = "test"
-#     }
-#   }
-#   type = "POSTGRESQL"
-# }
+  credentials {
+    credential_pair {
+      username = aws_db_instance.test.username
+      password = aws_db_instance.test.password
+    }
+  }
+  vpc_connection_properties {
+    vpc_connection_arn = aws_quicksight_vpc_connection.example.arn
+  }
+  parameters {
+    rds {
+      database    = "dvdrental"
+      instance_id = "test"
+    }
+  }
+  type = "POSTGRESQL"
+}
